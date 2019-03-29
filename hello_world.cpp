9242ec9da9ccbcdaf4dcc6b20e4819650a532531
PlotnikovAleksey <alex280202@gmail.com>
2019-03-30 00:32:09 +0300

    deleted using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int main(
   string name;
-  getline(cin, name);
-  cout<<"Hello world from "<<name;
+  getline(std::cin, name);
+  std::cout<<"Hello world from "<<name;
   return 0;
 }
 
